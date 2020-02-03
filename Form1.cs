c1919eca8f42e38647f04b707d1594fafd0d3d4f
behemoth13718 <36488167+behemoth13718@users.noreply.github.com>
2020-02-03 23:39:17 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using System.Configuration;
+using Newtonsoft.Json;
+
+namespace clear
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var DIR = new DirectoryInfo(ConfigurationManager.AppSettings["pathToDirectoryFiles"]);
+            var dicPathExt = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(ConfigurationManager.AppSettings["fileWithPathExt"]));
+
+            foreach (FileInfo file in DIR.GetFiles())
+            {
+                foreach (KeyValuePair<string, string> kvp in dicPathExt)
+                {
+                    var dicPath = new Dictionary<string, string>();
+                    string[] extention = kvp.Key.Split(',');
+                    foreach (var ext in extention)
+                        dicPath[ext] = kvp.Value;
+                    if (dicPath.TryGetValue(file.Extension, out string outputPath))
+                    {
+                        if (outputPath == "Delete")
+                        {
+                            File.Delete(file.FullName);
+                            break;
+                        }
+                        var filePath = Path.Combine(outputPath, file.Name);
+                        try
+                        {
+                            if (File.Exists(filePath)) File.Delete(filePath);
+                            File.Move(file.FullName, filePath);
+                        }
+                        catch (System.IO.IOException ex)
+                        {
+                            label1.Text = filePath + ex.Message;
+                        }
+                    }
+                }
+        }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace clear
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            if (ConfigurationManager.AppSettings["hidden"] == "False" | ConfigurationManager.AppSettings["hidden"] == "false")
+            {
+                Application.Run(new Form1());
+            }
+            else { Clear(); }
+
+            void Clear()
+            {
+                var DIR = new DirectoryInfo(ConfigurationManager.AppSettings["pathToDirectoryFiles"]);
+                var dicPathExt = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(ConfigurationManager.AppSettings["fileWithPathExt"]));
+
+                foreach (FileInfo file in DIR.GetFiles())
+                {
+                    foreach (KeyValuePair<string, string> kvp in dicPathExt)
+                    {
+                        var dicPath = new Dictionary<string, string>();
+                        string[] extention = kvp.Key.Split(',');
+                        foreach (var ext in extention)
+                            dicPath[ext] = kvp.Value;
+                        if (dicPath.TryGetValue(file.Extension, out string outputPath))
+                        {
+                            if (outputPath == "Delete")
+                            {
+                                File.Delete(file.FullName);
+                                break;
+                            }
+                            var filePath = Path.Combine(outputPath, file.Name);
+                            try
+                            {
+                                if (File.Exists(filePath)) File.Delete(filePath);
+                                File.Move(file.FullName, filePath);
+                            }
+                            catch (System.IO.IOException){}
+                        }
+                    }
+                }
+            }
+            
+            
+        }
+    }
+}
